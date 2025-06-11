91494fbd5c6847c672e4d1d8347417be8621f345
Raizkin <148555404+Raizkin@users.noreply.github.com>
2025-06-11 15:04:10 +0300

    4234234

--- js/main.js
+++ js/main.js
@@ -15,24 +15,77 @@ user.mood = "happy";
 user.hobby = "skydiving";
 user.premium = false;
 
-for (let key of Object.keys(user)) {
-  console.log(`${key}: ${user[key]}`);
-}
+console.log(user);
 
+const perebir = Object.keys(user);
+console.log(perebir);
+
+for (const i of perebir) {
+  console.log(`${i}:${user[i]}`);
+}
 
 // Напиши функцію countProps(obj), яка рахує кількість властивостей в об'єкті. Функція повертає число — кількість властивостей.
 
-function countProps(obj) {
-  return Object.keys(obj).length;
-}
+const countProps = (obj) => Object.keys(obj).length;
+
+console.log(countProps(user));
 
 
-const userFirst = {
-  name: "Artem",
-  age: 13,
-  hobby: "skydiving",
-  premium: false,
-  mood: "happy",
+// Напиши функцію findBestEmployee(employees), яка приймає об'єкт співробітників і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). Співробітники і кількість виконаних завдань містяться як властивості об'єкта в форматі "ім'я":"кількість задач".
+
+
+
+// Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. Функція рахує загальну суму зарплати працівників і повертає її. Кожне поле об'єкта, переданого в функцію, має вигляд "ім'я":"зарплата".
+
+const countTotalSalary = (employees) => {
+  let totalSum = 0;
+
+
+  for (const salary of Object.values(employees)) {
+    totalSum += salary;
+  }
+
+  return totalSum;
 };
 
-console.log(countProps(userFirst));
+
+const salaries = {
+  anton: 1000,
+  david: 2000,
+  Artem: 3000,
+  Olexander: 4000,
+};
+
+console.log(countTotalSalary(salaries));
+
+
+
+// Напиши функцію calculateTotalPrice(allProdcuts, productName), яка отримує масив об'єктів та ім'я продукту (значення властивості name). Повертає загальну вартість продукту (ціна * кількість).
+
+
+
+// Викличи функції для перевірки працездатності твоєї реалізації.
+
+
+
+
+const calculateTotalPrice = (allProducts, productName) => {
+  let totalSumma = 0;
+  for (const { product, price, amount } of allProducts) {
+    if (product === productName) {
+      totalSumma += price * amount;
+  }
+  }
+  return totalSumma
+}
+const products = [
+  { product: "apple", price: 10, amount: 2 },
+  { product: "banana", price: 5, amount: 3 },
+  { product: "bread", price: 20, amount: 1 },
+  { product: "fanta", price: 10, amount: 1 },
+];
+
+console.log(calculateTotalPrice(products, "bread"));
+console.log(calculateTotalPrice(products, "banana"));
+console.log(calculateTotalPrice(products, "apple"));
+console.log(calculateTotalPrice(products, "fanta"));
